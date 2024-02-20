1bfa58336afa5974f362780fe9df44939cf3461e
Brad Siegel <blsiege@gmail.com>
2024-02-20 18:26:08 -0500

    First commit

---
+++ GHFollowers/FavoriteListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteListVC.swift
+//  GHFollowers
+//
+//  Created by Brad Siegel on 2/20/24.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Brad Siegel on 2/20/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false // need this when using autolayout / constraints in code
+    }
+}
